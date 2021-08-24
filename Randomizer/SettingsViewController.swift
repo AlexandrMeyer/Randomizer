8779586dcfb1f9aee67cf8991e74bcc34984cb6c
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-08-24 18:03:16 +0300

    Done

---
+++ Randomizer/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Александр on 23.08.21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = maximumValue
+    }
+    
+    @IBAction func cancelButtonTapped() {
+    dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+   
+
+}
